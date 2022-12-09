4a4e6b4b2e6d7f883e4853ae0cad3f21829ffa1c
Toni Sredanović <toni@krakensystems.co>
2022-12-09 14:13:54 +0100

    day 8 and day 9 part a

--- solvers/y2022d08.py
+++ solvers/y2022d08.py
@@ -1,10 +1,204 @@
 from basesolver import BaseSolver
 
 
+class Grid:
+    def __init__(self, w, h) -> None:
+        self.w = w
+        self.h = h
+        self.trees = {}
+    
+    def add_tree(self, tree):
+        if tree.pos in self.trees.keys():
+            print('Tree already at position {}!!!'.format(tree.pos))
+            exit()
+        self.trees[tree.pos] = tree
+    
+    def get_tree(self, x, y):
+        return self.trees[(x, y)]
+
+    def is_tree_left_visible(self, tree):
+        if tree.x == 0:
+            return True
+        
+        for x in range(tree.x):
+            cmp_tree = self.get_tree(x, tree.y)
+            if cmp_tree.h >= tree.h:
+                return False
+
+        return True
+
+    def is_tree_right_visible(self, tree):
+        if tree.x == self.w - 1:
+            return True
+        
+        for x in range(tree.x+1, self.w):
+            cmp_tree = self.get_tree(x, tree.y)
+            if cmp_tree.h >= tree.h:
+                return False
+
+        return True
+
+    def is_tree_top_visible(self, tree):
+        if tree.y == 0:
+            return True
+        
+        for y in range(tree.y):
+            cmp_tree = self.get_tree(tree.x, y)
+            if cmp_tree.h >= tree.h:
+                return False
+        
+        return True
+
+    def is_tree_bottom_visible(self, tree):
+        if tree.y == self.h - 1:
+            return True
+        
+        for y in range(tree.y+1, self.h):
+            cmp_tree = self.get_tree(tree.x, y)
+            if cmp_tree.h >= tree.h:
+                return False
+
+        return True
+
+    def is_tree_visible(self, tree):
+        if self.is_tree_left_visible(tree) or self.is_tree_right_visible(tree) or self.is_tree_top_visible(tree) or self.is_tree_bottom_visible(tree):
+            return True
+        
+        return False
+
+    def visible_trees_count(self):
+        count = 0
+        for j in range(self.h):
+            for i in range(self.w):
+                tree = self.get_tree(i, j)
+                if self.is_tree_visible(tree):
+                    count += 1
+        return count
+
+    def tree_left_score(self, tree):
+        score = 0
+        if tree.x == 0:
+            return score
+
+        for x in reversed(range(tree.x)):
+            score += 1
+            cmp_tree = self.get_tree(x, tree.y)
+            if cmp_tree.h >= tree.h:
+                return score
+
+        return score
+
+    def tree_right_score(self, tree):
+        score = 0
+        if tree.x == self.w - 1:
+            return score
+        
+        for x in range(tree.x+1, self.w):
+            score += 1
+            cmp_tree = self.get_tree(x, tree.y)
+            if cmp_tree.h >= tree.h:
+                return score
+
+        return score
+
+    def tree_top_score(self, tree):
+        score = 0
+        if tree.y == 0:
+            return score
+        
+        for y in reversed(range(tree.y)):
+            score += 1
+            cmp_tree = self.get_tree(tree.x, y)
+            if cmp_tree.h >= tree.h:
+                return score
+        
+        return score
+
+    def tree_bottom_score(self, tree):
+        score = 0
+        if tree.y == self.h - 1:
+            return score
+        
+        for y in range(tree.y+1, self.h):
+            score += 1
+            cmp_tree = self.get_tree(tree.x, y)
+            if cmp_tree.h >= tree.h:
+                return score
+
+        return score
+
+    def tree_score(self, tree):
+        return self.tree_left_score(tree) * self.tree_right_score(tree) * self.tree_top_score(tree) * self.tree_bottom_score(tree)
+
+
+    def print(self):
+        line = ''
+        for j in range(self.h):
+            for i in range(self.w):
+                tree = self.get_tree(i, j)
+                line += str(tree.h)
+            print(line)
+            line = ''
+    
+    def print_visibility(self):
+        line = ''
+        for j in range(self.h):
+            for i in range(self.w):
+                tree = self.get_tree(i, j)
+                line += '1' if self.is_tree_visible(tree) else '0'
+            print(line)
+            line = ''
+
+    def print_scores(self):
+        line = ''
+        for j in range(self.h):
+            for i in range(self.w):
+                tree = self.get_tree(i, j)
+                line += str(self.tree_score(tree))
+            print(line)
+            line = ''
+
+class Tree:
+    def __init__(self, x, y, h) -> None:
+        self.x = x
+        self.y = y
+        self.h = int(h)
+
+    @property
+    def pos(self):
+        return (self.x, self.y)
+    
+    def __str__(self) -> str:
+        return '({}, {}) -> {}'.format(self.x, self.y, self.h)
+    
+    def __repr__(self) -> str:
+        return str(self)
+
+
 class Y2022D08Solver(BaseSolver):
     def solve_part_a(self):
-        return None
+        grid = Grid(len(self.lines[0]), len(self.lines))
+
+        for j, line in enumerate(self.lines):
+            for i, h in enumerate(line):
+                tree = Tree(i, j, h)
+                grid.add_tree(tree)
+        
+        return grid.visible_trees_count()
     
 
     def solve_part_b(self):
-        return None
+        grid = Grid(len(self.lines[0]), len(self.lines))
+
+        for j, line in enumerate(self.lines):
+            for i, h in enumerate(line):
+                tree = Tree(i, j, h)
+                grid.add_tree(tree)
+        
+        max_tree_score = 0
+        for tree in grid.trees.values():
+            tree_score = grid.tree_score(tree)
+            if tree_score > max_tree_score:
+                max_tree_score = tree_score
+        
+        return max_tree_score
--- solvers/y2022d09.py
+++ solvers/y2022d09.py
@@ -1,9 +1,131 @@
 from basesolver import BaseSolver
 
+def calc_points_diff(point_1, point_2):
+    return point_1.x - point_2.x, point_1.y - point_2.y
+
+def calc_points_touching(point_1, point_2):
+    points_diff_x, points_diff_y = calc_points_diff(point_1, point_2)
+
+    if abs(points_diff_x) <= 1 and abs(points_diff_y) <= 1:
+        return True
+    
+    return False
+
+class Point:
+    def __init__(self, x, y, mark='-') -> None:
+        self.x = x
+        self.y = y
+        self.mark = mark
+    
+    @property
+    def pos(self):
+        return (self.x, self.y)
+    
+    def __str__(self) -> str:
+        return str(self.pos)
+    
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class Grid:
+    def __init__(self) -> None:
+        self.start_point = Point(0, 0, 's')
+
+        self.head_points = [Point(0, 0, 'H')]
+
+        self.tail_points = [Point(0, 0, 'T')]
+
+    def move(self, move_line):
+        direction = move_line.split(' ')[0]
+        step_count = int(move_line.split(' ')[1])
+
+        for _ in range(step_count):
+            current_head_point = self.head_points[-1]
+            if direction == 'R':
+                next_head_point = Point(current_head_point.x + 1, current_head_point.y, 'H')
+            elif direction == 'L':
+                next_head_point = Point(current_head_point.x - 1, current_head_point.y, 'H')
+            elif direction == 'U':
+                next_head_point = Point(current_head_point.x, current_head_point.y + 1, 'H')
+            elif direction == 'D':
+                next_head_point = Point(current_head_point.x, current_head_point.y - 1, 'H')
+
+            current_tail_point = self.tail_points[-1]
+            if calc_points_touching(next_head_point, current_tail_point):
+                next_tail_point = Point(current_tail_point.x, current_tail_point.y, 'T')
+            else:
+                points_diff_x, points_diff_y = calc_points_diff(next_head_point, current_tail_point)
+
+                if points_diff_x == 0: # On same column, must move y
+                    x_to_add = 0
+                    if points_diff_y == 2:
+                        y_to_add = 1
+                    elif points_diff_y == -2:
+                        y_to_add = -1
+                    else:
+                        y_to_add = 0
+
+                elif points_diff_y == 0: # On same row, must move x
+                    y_to_add = 0
+                    if points_diff_x == 2:
+                        x_to_add = 1
+                    elif points_diff_x == -2:
+                        x_to_add = -1
+                    else:
+                        x_to_add = 0
+
+                else:  # On diff row and column, must move diagonally
+                    x_to_add = 1 if points_diff_x >= 0 else -1
+                    y_to_add = 1 if points_diff_y >= 0 else -1
+                next_tail_point = Point(current_tail_point.x + x_to_add, current_tail_point.y + y_to_add, 'T')
+
+            self.head_points.append(next_head_point)
+            self.tail_points.append(next_tail_point)
+    
+    @property
+    def w(self):
+        return max([point.x for point in self.head_points + self.tail_points]) + 1
+
+    @property
+    def h(self):
+        return max([point.y for point in self.head_points + self.tail_points]) + 1
+    
+
+    def print_points_at(self, i):
+        head_point = self.head_points[i]
+        tail_point = self.tail_points[i]
+
+        line = ''
+        for j in range(self.h):
+            for i in range(self.w):
+                pos = (i, j)
+
+                point_at_pos = None
+                if pos == self.start_point.pos:
+                    point_at_pos = self.start_point
+                if pos == tail_point.pos:
+                    point_at_pos = tail_point
+                if pos == head_point.pos:
+                    point_at_pos = head_point
+
+                line += str(point_at_pos.mark) if point_at_pos else '.'
+
+            print(line)
+            line = ''
+
+    def print(self):
+        for i in range(len(self.head_points)):
+            self.print_points_at(i)
+            print()
+
 
 class Y2022D09Solver(BaseSolver):
     def solve_part_a(self):
-        return None
+        grid = Grid()
+        for line in self.lines:
+            grid.move(line)
+        return len(set([point.pos for point in grid.tail_points]))
     
 
     def solve_part_b(self):
