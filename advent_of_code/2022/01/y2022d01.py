9560f37bbc1cb72024ce4b0861f99b47b1619b0b
Toni Sredanović <toni@krakensystems.co>
2022-12-09 22:47:46 +0100

    empty solvers

---
+++ advent_of_code/2022/01/y2022d01.py
@@ -0,0 +1,10 @@
+from advent_of_code.basesolver import BaseSolver
+
+
+class Y2022D01Solver(BaseSolver):
+    def solve_part_a(self):
+        return None
+    
+
+    def solve_part_b(self):
+        return None
