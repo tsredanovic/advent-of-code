6d8f60692ada63a700c66ce5685a48d65e99ca45
Toni Sredanović <toni@krakensystems.co>
2022-12-09 20:36:46 +0100

    delete 2018, add args, install dotenv

---
+++ advent_of_code/2022/01/y2022d01.py
@@ -0,0 +1,17 @@
+from basesolver import BaseSolver
+
+
+class Y2022D01Solver(BaseSolver):
+    def solve_part_a(self):
+        elf_sums = []
+        for chunk in self.chunks(type=int):
+            elf_sums.append(sum(chunk))
+        return max(elf_sums)
+    
+
+    def solve_part_b(self):
+        elf_sums = []
+        for chunk in self.chunks(type=int):
+            elf_sums.append(sum(chunk))
+        elf_sums.sort(reverse=True)
+        return elf_sums[0] + elf_sums[1] + elf_sums[2]
--- advent_of_code/__init__.py
+++ advent_of_code/__init__.py
@@ -1,2 +1,11 @@
+import argparse
+
 def solve():
-    print('Hello world')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('year', type=int, help='year')
+    parser.add_argument('day', type=int, help='day')
+    parser.add_argument('part', type=str, help='part')
+
+    args = parser.parse_args()
+    print(args.year, args.day, args.part)
+
