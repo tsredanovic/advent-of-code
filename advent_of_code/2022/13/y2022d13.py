c599a9d80d7130d7fecd7b83bf050345420ef37f
Toni Sredanović <toni@krakensystems.co>
2022-12-18 20:39:05 +0100

    13 and 14

--- advent_of_code/2022/13/y2022d13.py
+++ advent_of_code/2022/13/y2022d13.py
@@ -1,9 +1,72 @@
+import json
 from advent_of_code.basesolver import BaseSolver
+
+# 1 - right order
+# -1 - wrong order
+def compare_lists(l1, l2):
+    #print('{} vs {}'.format(l1, l2))
+
+    l1_len = len(l1)
+    l2_len = len(l2)
+
+    for i in range(max(l1_len, l2_len)):
+        if i == len(l1): # l1 ran out of items
+            return 1
+        elif i == len(l2): # l2 ran out of items
+            return -1
+        else:
+            v1 = l1[i]
+            v2 = l2[i]
+            if isinstance(v1, list) or isinstance(v2, list):
+                if not isinstance(v1, list):
+                    v1 = [v1]
+                if not isinstance(v2, list):
+                    v2 = [v2]
+                result = compare_lists(v1, v2)
+                if not result is None:
+                    return result
+            else:
+                if v1 < v2:
+                    return 1
+                elif v2 < v1:
+                    return -1
 
 
 class Y2022D13Solver(BaseSolver):
     def solve_part_a(self):
-        return None
+        result = 0
+        for i, pair in enumerate(self.chunks(), 1):
+            pair_result = compare_lists(json.loads(pair[0]), json.loads(pair[1]))
+            #print('PAIR {}: {}'.format(i, pair_result))
+            if pair_result == 1:
+                result += i
+        return result
 
     def solve_part_b(self):
-        return None
+        packets = [json.loads(line) for line in self.lines if line]
+        packets.append(json.loads('[[2]]'))
+        packets.append(json.loads('[[6]]'))
+        iteration = 0
+        while iteration == 0 or swap_happened:
+            #print('ITERATION {}:'.format(iteration))
+            #import pprint
+            #pprint.pprint(packets, indent=2)
+            swap_happened = False
+            for i in range(1, len(packets)):
+                left_packet = packets[i-1]
+                right_packet = packets[i]
+                compare_result = compare_lists(left_packet, right_packet)
+                #print('{} vs {} -> {}'.format(left_packet, right_packet, compare_result))
+                if  compare_result == -1:
+                    packets[i-1] = right_packet
+                    packets[i] = left_packet
+                    swap_happened = True
+                #print(swap_happened)
+            iteration += 1
+
+        result = 1
+        for i, packet in enumerate(packets, 1):
+            if packet == [[2]] or packet == [[6]]:
+                result *= i
+
+        return result
--- advent_of_code/2022/14/y2022d14.py
+++ advent_of_code/2022/14/y2022d14.py
@@ -1,9 +1,154 @@
 from advent_of_code.basesolver import BaseSolver
 
+MAX_INT = 2**10000
+
+class Sand:
+    def __init__(self, start_point, rocks, floor_exists=False) -> None:
+        self.start_point = start_point
+
+        self.rocks = rocks
+        self.rocks_points = {}
+        for rock in rocks:
+            for point in rock.points.values():
+                self.rocks_points[point.pos] = point
+
+        self.rocks_min_x = MAX_INT
+        self.rocks_max_x = -MAX_INT
+        self.rocks_min_y = MAX_INT
+        self.rocks_max_y = -MAX_INT
+        for point in self.rocks_points.values():
+            if point.x < self.rocks_min_x:
+                self.rocks_min_x = point.x
+            if point.x > self.rocks_max_x:
+                self.rocks_max_x = point.x
+            if point.y < self.rocks_min_y:
+                self.rocks_min_y = point.y
+            if point.y > self.rocks_max_y:
+                self.rocks_max_y = point.y
+        
+        self.floor_y = self.rocks_max_y + 2
+
+        self.grains_points = {}
+
+        self.floor_exists = floor_exists
+    
+    def drop_sand(self):
+        while True:
+            final_grain_point = self.drop_grain()
+            if not final_grain_point:
+                break
+            self.grains_points[final_grain_point.pos] = final_grain_point
+            if final_grain_point.pos == self.start_point.pos:
+                break
+            #self.print()
+
+    def drop_grain(self):
+        current_point = Point(self.start_point.x, self.start_point.y)
+        #print(current_point)
+        while True:
+            possible_next_points = [
+                Point(current_point.x, current_point.y + 1),
+                Point(current_point.x - 1, current_point.y + 1),
+                Point(current_point.x + 1, current_point.y + 1),
+            ]
+            next_point = None
+            for possible_next_point in possible_next_points:
+                is_rock_at_possible_next_point = True if self.rocks_points.get(possible_next_point.pos) else False
+                is_grain_at_possible_next_point = True if self.grains_points.get(possible_next_point.pos) else False
+                #print(possible_next_point, is_rock_at_possible_next_point)
+                if not is_rock_at_possible_next_point and not is_grain_at_possible_next_point:
+                    next_point = possible_next_point
+                    break
+            if not next_point:
+                return current_point
+            if next_point.y == self.floor_y:
+                if not self.floor_exists:
+                    return None
+                else:
+                    return current_point
+            current_point = next_point
+    
+    def print(self):
+        lines = []
+        line = ""
+        for y in range(self.start_point.y, self.floor_y + 1):
+            for x in range(self.rocks_min_x, self.rocks_max_x + 1):
+                pos = (x, y)
+                char = '.'
+                if self.grains_points.get(pos):
+                    char = 'o'
+                elif self.start_point.pos == pos:
+                    char = '+'
+                elif self.rocks_points.get(pos):
+                    char = '#'
+                elif y == self.floor_y:
+                    char = '#'
+                line += char
+            lines.append(line)
+            line = ""
+        print("\n".join(lines))
+
+
+class Rock:
+    def __init__(self, points) -> None:
+        self.points = points
+
+    def rocks_from_lines(lines):
+        rocks = []
+        for line in lines:
+            rocks.append(Rock.rock_from_line(line))
+        return rocks
+    
+    def rock_from_line(line):
+        rock_edges = [Point(int(point_str.split(',')[0]), int(point_str.split(',')[1])) for point_str in line.split(' -> ')]
+        points = {}
+        for i in range(1, len(rock_edges)):
+            rock_edge_1 = rock_edges[i-1]
+            rock_edge_2 = rock_edges[i]
+            if rock_edge_1.x == rock_edge_2.x:
+                min_y = min(rock_edge_1.y, rock_edge_2.y)
+                max_y = max(rock_edge_1.y, rock_edge_2.y)
+                for y in range(min_y, max_y + 1):
+                    point = Point(rock_edge_1.x, y)
+                    if point.pos not in points.keys():
+                        points[point.pos] = point
+            if rock_edge_1.y == rock_edge_2.y:
+                min_x = min(rock_edge_1.x, rock_edge_2.x)
+                max_x = max(rock_edge_1.x, rock_edge_2.x)
+                for x in range(min_x, max_x + 1):
+                    point = Point(x, rock_edge_1.y)
+                    if point.pos not in points.keys():
+                        points[point.pos] = point
+        return Rock(points)
+
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    @property
+    def pos(self):
+        return (self.x, self.y)
+
+    def __str__(self) -> str:
+        return str(self.pos)
+
+    def __repr__(self) -> str:
+        return str(self)
 
 class Y2022D14Solver(BaseSolver):
     def solve_part_a(self):
-        return None
+        start_point = Point(500, 0)
+        rocks = Rock.rocks_from_lines(self.lines)
+        sand = Sand(start_point, rocks)
+        #sand.print()
+        sand.drop_sand()
+        return len(sand.grains_points)
 
     def solve_part_b(self):
-        return None
+        start_point = Point(500, 0)
+        rocks = Rock.rocks_from_lines(self.lines)
+        sand = Sand(start_point, rocks, floor_exists=True)
+        #sand.print()
+        sand.drop_sand()
+        return len(sand.grains_points)
